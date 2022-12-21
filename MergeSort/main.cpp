39bda6bff0e95faf6e8d79df8573f878f6cb2248
1Misaka <majialin7@outlook.com>
2022-12-21 14:11:25 +0800

    update

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -1,53 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
-void merge(int *arr, int start1, int end1, int start2, int end2) {
+void merge(int* arr, int start1, int end1, int start2, int end2) {
 	int* B;
-	int n = (end2 - start1 + 1);
-	int i, j, k;
+	int n = end2 - start1 + 1;
 	B = (int*)malloc(n * sizeof(int));
-	for ( i = start1,j=start1,k=start2; j<=end1&& k<= end2; ++i)
+	int i = start1, j = start2, k = 0;
+	for (i, j, k; i <= end1 && j <= end2; )
 	{
-		if (arr[j] <= arr[k]) {
-			B[i] = arr[j];
-			++j;
-		}
-		else
-		{
-			B[i] = arr[k];
-			++k;
-		}
+		if (arr[i] <= arr[j]) { B[k] = arr[i]; ++i, ++k; }
+		if (arr[i] > arr[j]) { B[k] = arr[j]; ++j, ++k; }
 	}
-	while (j<=end1)
-	{
-		B[i] = arr[j];
-		++i, ++j;
-	}
-	while (k <= end2)
-	{
-		B[i] = arr[k];
-		++i, ++k;
-	}
-
-	for (int i = start1; i <= end2; ++i)
-	{
-		arr[i] = B[i];
-	}
-	//free(B);
+	while (i <= end1) { B[k] = arr[i]; ++i, ++k; }
+	while (j <= end2) { B[k] = arr[j]; ++j, ++k; }
+	for (int i = start1, j = 0; i <= end2; ++i, ++j) { arr[i] = B[j]; }
 }
 
-void MergeSort(int arr[], int low, int high) {
-	if(low<high){
-		int mid = (low + high)/2;
-		MergeSort(arr,low,mid);
-		MergeSort(arr,mid+1,high);
-		merge(arr,low,mid,mid+1,high);
+void MergeSort(int* arr, int low, int high) {
+	if (low < high) {
+		int mid = (low + high) / 2;
+		MergeSort(arr, low, mid);
+		MergeSort(arr, mid + 1, high);
+		merge(arr, low, mid, mid + 1, high);
 	}
 }
-
-int main(){
-	int arr[]={1,9,2,8,3,7,4,6,5};
+int main() {
+	int arr[] = { 26,37,16,18,39,66,46,15,13,48 };
 	int n = sizeof(arr) / sizeof(int);
 	MergeSort(arr, 0, n - 1);
 	return 0;
--- newTest/main.cpp
+++ newTest/main.cpp
@@ -1,4 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void merge(int *arr, int start1, int end1, int start2, int end2) {
+	int* B;
+	int n = end2 - start1 + 1;
+	B = (int*)malloc(n * sizeof(int));
+	int i = start1, j = start2, k = 0;
+	for (i,j,k; i<=end1 && j<=end2; )
+	{
+		if (arr[i] <= arr[j]) { B[k] = arr[i]; ++i, ++k; }
+		if (arr[i] > arr[j]) { B[k] = arr[j]; ++j, ++k; }
+	}
+	while (i <= end1) { B[k] = arr[i]; ++i, ++k; }
+	while (j <= end2) { B[k] = arr[j]; ++j, ++k; }
+	for(int i=start1,j=0;i<=end2;++i,++j){	arr[i] = B[j];	}
+}
 
+void MergeSort(int* arr, int low, int high) {
+	if (low < high) {
+		int mid = (low + high)/2;
+		MergeSort(arr, low, mid);
+		MergeSort(arr, mid + 1, high);
+		merge(arr, low, mid, mid + 1,high);
+	}
+}
+int main() {
+	int arr[] = { 1,3,5,7,9,2,4,6,8,10 };
+	int n = sizeof(arr) / sizeof(int);
+	MergeSort(arr, 0, n - 1);
+	return 0;
+}
